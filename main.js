68296aaa9f3562048e378c092bd29e3403dab08b
wesleyoliveira01 <wesleyoliveira.dev@outlook.com>
2023-03-03 12:13:11 -0300

    função de atualizar pagina adicionada

--- main.js
+++ main.js
@@ -1,30 +1,24 @@
+const getKanye = "https://api.kanye.rest";
 
-const getKanye = 'https://api.kanye.rest'
+const div = document.createElement("div");
+div.classList.add("citacao");
 
-const div = document.createElement('div')
-div.classList.add('citacao')
+async function kanyeFunction(api) {
+  const kanyeQuote = await fetch(api);
 
+  const quote = await kanyeQuote.json();
 
+  const container = document.querySelector(".quote");
+  const box = document.querySelector(".container");
 
+  console.log(quote.quote);
 
+  div.innerHTML = quote.quote;
 
+  container.appendChild(div);
 
-async function kanyeFunction (api){
-
-    const kanyeQuote = await fetch(api)
-
-    const quote = await kanyeQuote.json()
-
-    const container = document.querySelector(".quote")
-
-    console.log(quote.quote)
-
-    div.innerHTML = quote.quote
-    
-    container.appendChild(div)
-
+  box.addEventListener("click", function () {
+    location.reload();
+  });
 }
-kanyeFunction(getKanye)
-
-
-
+kanyeFunction(getKanye);
